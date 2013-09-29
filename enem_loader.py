24c5ae2bcf49e1333db4c4ad7ea8e768808cf3f3
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-09-29 00:50:07 -0300

    Refactorings

--- enem_loader.py
+++ enem_loader.py
@@ -84,91 +84,6 @@ class State(Document, DocumentHelpers):
     def all(cls):
         return cls.objects.order_by('abbreviation')
 
-def parse_line(line):
-    def get_string(line, start, size, emptyValue = '.'):
-        value = line[start: start + size].strip()
-
-        return value if value != emptyValue else None
-
-    present_in_exam = { ka: (int(get_string(line, 532 + i, 1)) == 1)                                   for i, ka in enumerate(KNOWLEDGE_AREAS) }
-    scores          = { ka: (float(get_string(line, 536 + i * 9, 9)) if present_in_exam[ka] else None) for i, ka in enumerate(KNOWLEDGE_AREAS) }
-    ranges          = { ka: (int(scores[ka] / 100.0001) + 1          if present_in_exam[ka] else None) for i, ka in enumerate(KNOWLEDGE_AREAS) }
-
-    city_name = get_string(line, 218, 150)
-    city_name = city_name and city_name.title()
-
-    return {
-        'scores': scores,
-        'ranges': ranges,
-        'subscription_code': get_string(line, 0, 12),
-        'year': int(get_string(line, 12, 4)),
-        'age': int(get_string(line, 16, 3)),
-        'gender': int(get_string(line, 19, 1)),
-        'school_code': get_string(line, 203, 8),
-        'city': {
-            'code': get_string(line, 211, 7),
-            'name': city_name
-        },
-        'state': get_string(line, 368, 2)
-    }
-
-def process_data_file(data_file, process_num):
-    print('[% 3d] Processing file %s...' % (process_num, data_file))
-
-    file_size = os.path.getsize(data_file)
-    total_lines = file_size / LINE_SIZE
-
-    with open(data_file) as f:
-        state = city = school = None
-
-        for i, line in enumerate(f):
-            if i % (total_lines / TOTAL_PRINTS) == 0: print(' ' * process_num * 4 + '[%d] %.1f%%' % (process_num, i * 100.0 / total_lines))
-            # if i > 10: break
-
-            row = parse_line(line)
-
-            # print(row)
-
-            # State non-existent or distinct from current state?
-            if not state or row['state'] != state.abbreviation:
-                if row['state']:
-                    state = get_or_create_state(row['state'])
-                else:
-                    state = None
-
-            # City non-existent or distinct from current city?
-            if not city or row['city']['code'] != city.code:
-                if row['city']['code']:
-                    city = get_or_create_city(row['city']['code'], { 'name': row['city']['name'], 'state': state })
-                else:
-                    city = None
-
-            # School non-existent or distinct from current school?
-            if not school or row['school_code'] != school.code:
-                if row['school_code']:
-                    school = get_or_create_school(
-                        row['school_code'],
-                        {
-                            'name': 'Escola ID=%s da cidade de %s-%s' % (row['school_code'], row['city']['name'], row['state']), 
-                            'city': city, 
-                            'stats': ScoreStatistics.create_empty()
-                        }
-                    )
-                else:
-                    school = None
-        
-            if school:
-                kwargs = {}
-                
-                for i, knowledge_area in enumerate(KNOWLEDGE_AREAS):
-                    range_value = row['ranges'][knowledge_area]
-    
-                    if range_value: 
-                        kwargs.update({ 'inc__stats__score_counts__%d__%d' % (i, range_value - 1): 1 })
-                
-                if kwargs != {}:
-                    School.find(id=school.id).update_one(**kwargs)
-
 if __name__ == '__main__':
     LINE_SIZE = 1180
     TOTAL_PRINTS = 40
@@ -187,6 +102,91 @@ if __name__ == '__main__':
     @Retry(100, delay=0.01, exceptions=(mongoengine.errors.NotUniqueError))
     def get_or_create_school(code, defaults):
         return School.objects.get_or_create(code=code, defaults=defaults)[0]
+
+    def parse_line(line):
+        def get_string(line, start, size, emptyValue = '.'):
+            value = line[start: start + size].strip()
+
+            return value if value != emptyValue else None
+
+        present_in_exam = { ka: (int(get_string(line, 532 + i, 1)) == 1)                                   for i, ka in enumerate(KNOWLEDGE_AREAS) }
+        scores          = { ka: (float(get_string(line, 536 + i * 9, 9)) if present_in_exam[ka] else None) for i, ka in enumerate(KNOWLEDGE_AREAS) }
+        ranges          = { ka: (int(scores[ka] / 100.0001) + 1          if present_in_exam[ka] else None) for i, ka in enumerate(KNOWLEDGE_AREAS) }
+
+        city_name = get_string(line, 218, 150)
+        city_name = city_name and city_name.title()
+
+        return {
+            'scores': scores,
+            'ranges': ranges,
+            'subscription_code': get_string(line, 0, 12),
+            'year': int(get_string(line, 12, 4)),
+            'age': int(get_string(line, 16, 3)),
+            'gender': int(get_string(line, 19, 1)),
+            'school_code': get_string(line, 203, 8),
+            'city': {
+                'code': get_string(line, 211, 7),
+                'name': city_name
+            },
+            'state': get_string(line, 368, 2)
+        }
+
+    def process_data_file(data_file, process_num):
+        print('[% 3d] Processing file %s...' % (process_num, data_file))
+
+        file_size = os.path.getsize(data_file)
+        total_lines = file_size / LINE_SIZE
+
+        with open(data_file) as f:
+            state = city = school = None
+
+            for i, line in enumerate(f):
+                if i % (total_lines / TOTAL_PRINTS) == 0: print(' ' * process_num * 4 + '[%d] %.1f%%' % (process_num, i * 100.0 / total_lines))
+                # if i > 10: break
+
+                row = parse_line(line)
+
+                # print(row)
+
+                # State non-existent or distinct from current state?
+                if not state or row['state'] != state.abbreviation:
+                    if row['state']:
+                        state = get_or_create_state(row['state'])
+                    else:
+                        state = None
+
+                # City non-existent or distinct from current city?
+                if not city or row['city']['code'] != city.code:
+                    if row['city']['code']:
+                        city = get_or_create_city(row['city']['code'], { 'name': row['city']['name'], 'state': state })
+                    else:
+                        city = None
+
+                # School non-existent or distinct from current school?
+                if not school or row['school_code'] != school.code:
+                    if row['school_code']:
+                        school = get_or_create_school(
+                            row['school_code'],
+                            {
+                                'name': 'Escola ID=%s da cidade de %s-%s' % (row['school_code'], row['city']['name'], row['state']), 
+                                'city': city, 
+                                'stats': ScoreStatistics.create_empty()
+                            }
+                        )
+                    else:
+                        school = None
+        
+                if school:
+                    kwargs = {}
+                
+                    for i, knowledge_area in enumerate(KNOWLEDGE_AREAS):
+                        range_value = row['ranges'][knowledge_area]
+    
+                        if range_value: 
+                            kwargs.update({ 'inc__stats__score_counts__%d__%d' % (i, range_value - 1): 1 })
+                
+                    if kwargs != {}:
+                        School.find(id=school.id).update_one(**kwargs)
 
     if len(sys.argv) < 3: 
         raise Exception('Usage: %s <db-name> <data-files>' % sys.argv[0])
