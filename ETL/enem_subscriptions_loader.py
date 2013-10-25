6282ccb023b16a06ec13da374ec031485f999171
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-10-25 01:37:07 -0200

    Loading subscription (facts) data

---
+++ ETL/enem_subscriptions_loader.py
@@ -0,0 +1,67 @@
+# coding=UTF-8
+
+# To import into MongoDB: time mongoimport --host localhost:27017 --db enem_subscriptions --collection subscriptions --drop <~/tmp/inep_data.json
+
+import sys, json, os
+
+KNOWLEDGE_AREAS = ['NAT', 'HUM', 'LAN', 'MAT']
+
+def parse_line(line):
+    def get_string(line, start, size, emptyValue = '.'):
+        value = line[start: start + size].strip()
+
+        return value if value != emptyValue else None
+
+    present_in_exam = { 
+        ka: (int(get_string(line, 532 + i, 1)) == 1)
+        for i, ka in enumerate(KNOWLEDGE_AREAS) 
+    }
+    scores = { 
+        ka: (float(get_string(line, 536 + i * 9, 9)) if present_in_exam[ka] else None) 
+        for i, ka in enumerate(KNOWLEDGE_AREAS) 
+    }
+    ranges = { 
+        ka: (int(scores[ka] / 100.0001) + 1 if present_in_exam[ka] else None) 
+        for i, ka in enumerate(KNOWLEDGE_AREAS) 
+    }
+
+    return {
+        'scores': scores,
+        'ranges': ranges,
+        'subscription_code': get_string(line, 0, 12),
+        'year': int(get_string(line, 12, 4)),
+        'age': int(get_string(line, 16, 3)),
+        'gender': int(get_string(line, 19, 1)),
+        'school_code': get_string(line, 203, 8),
+        'city': {
+            'code': get_string(line, 211, 7),
+            'name': get_string(line, 218, 150)
+        },
+        'state': get_string(line, 368, 2)
+    }
+
+if __name__ == '__main__':
+    LINE_SIZE    = 1180
+    TOTAL_PRINTS = 100
+
+    if len(sys.argv) < 2:
+        raise Exception('Usage: %s <data-file>' % sys.argv[0])
+    
+    data_file = sys.argv[1]
+
+    file_size = os.path.getsize(data_file)
+    total_lines = file_size / LINE_SIZE
+
+    with open("/Users/cassiano/tmp/inep_data.json", "w") as outfile:
+        with open(data_file) as infile:
+            for i, line in enumerate(infile):
+                if i % (total_lines / TOTAL_PRINTS) == 0 or i == total_lines - 1: 
+                    progress = i * 100.0 / (total_lines - 1)
+                
+                    print('%.1f%%' % progress)
+                
+                # if i > 10: break
+
+                row = parse_line(line)
+
+                outfile.write(json.dumps(row) + '\n')
