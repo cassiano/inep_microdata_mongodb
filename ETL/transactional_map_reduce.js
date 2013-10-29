252e75021f970611ca3636fee2d4be856963bf4c
Cassiano D'Andrea <cassiano.dandrea@tagview.com.br>
2013-10-29 00:19:38 -0200

    Generalizing JS map-reduce queries

--- ETL/transactional_map_reduce.js
+++ ETL/transactional_map_reduce.js
@@ -1,5 +1,5 @@
 // PS: locality can be either a school, city, state or even the whole country!
-var reduceFunction = function(locality, values) {
+function reduceFunction(locality, values) {
     var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
     for (var i = 0; i < values.length; i++) {
@@ -11,37 +11,49 @@ var reduceFunction = function(locality, values) {
     return { counts: counts };
 };
 
-var mapFunction = function() {
+function mapFunction() {
     var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-    counts[this.ranges['NAT'] - 1] = 1;
+    counts[this.ranges[data.enemKnowledgeArea] - 1] = 1;
 
     emit('locality', { counts: counts });
 };
 
-var sp_city_aggregates = db.subscriptions.mapReduce(
-    mapFunction,
-    reduceFunction,
-    {
-         query: { 'city.code': '3550308', year: 2011, 'ranges.NAT': { $ne: null } },
-         out: { inline: 1 }
-    }
-);
+function mapReduceFn(query, enemKnowledgeArea) {
+	return db.subscriptions.mapReduce(
+	    mapFunction,
+	    reduceFunction,
+	    {
+	         query: query,
+	         out: { inline: 1 },
+			 scope: {
+				 data: { enemKnowledgeArea: enemKnowledgeArea }
+			 }
+	    }
+	);
+};
 
-var sp_state_aggregates = db.subscriptions.mapReduce(
-    mapFunction,
-    reduceFunction,
-    {
-         query: { state: 'SP', year: 2011, 'ranges.NAT': { $ne: null } },
-         out: { inline: 1 }
-    }
-);
+function cityAggregates(city_code, year, enemKnowledgeArea) {
+	var query = { 'city.code': city_code, year: year };
+	query['ranges.' + enemKnowledgeArea] = { $ne: null };
 
-var br_aggregates = db.subscriptions.mapReduce(
-    mapFunction,
-    reduceFunction,
-    {
-         query: { year: 2011, 'ranges.NAT': { $ne: null } },
-         out: { inline: 1 }
-    }
-);
+	return mapReduceFn(query, enemKnowledgeArea);
+};
+
+function stateAggregates(stateAbbreviation, year, enemKnowledgeArea) {
+	var query = { state: stateAbbreviation, year: year };
+	query['ranges.' + enemKnowledgeArea] = { $ne: null };
+
+	return mapReduceFn(query, enemKnowledgeArea);
+};
+
+function countryAggregates(year, enemKnowledgeArea) {
+	var query = { year: year };
+	query['ranges.' + enemKnowledgeArea] = { $ne: null };
+
+	return mapReduceFn(query, enemKnowledgeArea);
+};
+
+var spCityAggregates = cityAggregates('3550308', 2011, 'NAT');
+var spStateAggregates = stateAggregates('SP', 2011, 'NAT');
+var brCountryAggregates = countryAggregates(2011, 'NAT');
